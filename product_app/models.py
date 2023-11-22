14b7423d897a3db4b5410c1bb5577b4a35f92c66
Asliddin <91380853+AsliddinWeb@users.noreply.github.com>
2023-11-22 16:36:21 +0500

    Product slogan added

--- product_app/models.py
+++ product_app/models.py
@@ -17,6 +17,7 @@ class Product(models.Model):
     body = models.TextField(null=True, blank=True)
     razmer = models.CharField(max_length=455, null=True, blank=True)
     image = models.ImageField(upload_to='products', null=True, blank=True)
+    filter = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.name
